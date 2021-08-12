06f7df534012fefeccef024b2c372046dbb221f3
David Ramirez Monroy <ramirezmonroyd@gmail.com>
2021-08-11 21:46:01 -0500

    Corregido un error en el punto fijo de la tercera funcion

--- Talleres/taller_1/c/punto_fijo_c.py
+++ Talleres/taller_1/c/punto_fijo_c.py
@@ -1,30 +1,24 @@
 import numpy as np
+from decimal import *
 
 
-def g(x):
+def f(x):
     return (x**3)-((2*x)**2)+((4*x)/3)-(8/27)
 
 
-tols = [10**-8, 10**-16, 10**-32, 10**-56]
+def g(x):
+    return 1/np.sin(x)
 
+
+tols = [10**-8, 10**-16, 10**-32, 10**-56]
 for tol in tols:
+    i = 0
     x = 0.7
-    x1 = g(x)
-    err = abs(x1 - x)
-    x2 = g(x1)
-    err = abs(x2 - x1)
-    x3 = x - ((x1 - x)**2) / (x2 - 2*x1 + x)
-    err = abs(x3 - x2)
-    i = 3
-    while err > tol:
-        x = x3
-        x1 = g(x)
-        err = abs(x1 - x)
-        x2 = g(x1)
-        err = abs(x2 - x1)
-        if((x2 - 2*x1 + x) != 0):
-            x3 = x - ((x1 - x)**2) / (x2 - 2*x1 + x)
-            err = abs(x3 - x2)
-        i += 3
+    err = np.abs(g(x) - x)
+    while(err > tol and i < 1000):
+        if i > 0:
+            err = np.abs(g(x) - x)
+        x = g(x)
+        i += 1
     print(
-        f'Los valores de la raiz es {x3} y {x3 * -1}, su error es {err}, y se hicieron {i} iteraciones')
+        f'Se obtuvieron las raices {Decimal(x)} y {Decimal(x * -1)} en {i} iteraciones, error {err}')
